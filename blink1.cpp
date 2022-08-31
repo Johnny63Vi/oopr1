c639e0e82b797550b5bd1b4630daa29ba9feb607
John vi <john63vi@yahoo.com>
2022-08-31 15:19:08 +0200

    opdracht1

---
+++ blink1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+#include "Led.h"
+
+#define TIMELEDON 1000
+#define TIMELEDOFF 500
+
+int main (void)
+{
+  cout<<"hoi opgave1"<<endl;
+  Led ld(18);
+
+  for (;;)
+  {
+    ld.zetAan();
+    usleep(1000000);
+    ld.zetUit();
+    usleep(500000);
+  }
+  return 0 ;
+}
