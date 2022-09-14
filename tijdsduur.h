a6211a98e27a841ab46016e5d486a7eebd8d18f3
John Visser <100942775+Johnny63Vi@users.noreply.github.com>
2022-09-14 16:18:32 +0200

    Update tijdsduur.h

--- tijdsduur.h
+++ tijdsduur.h
@@ -5,20 +5,11 @@
 class Tijdsduur {
 public:
 //...
-	Tijdsduur();
-	Tijdsduur(int m);
-	Tijdsduur(int u, int m);
     void eraf(const Tijdsduur& t);
-    void erbij(const Tijdsduur& t);
-    void maal(int t);
-    void print() const;
-    int deTimerTijd()const;
 
 //...
 
 private:	
-    void normaliseer();
-    int min;
     int sec;
 //...
 };
