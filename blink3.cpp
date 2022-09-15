b6b69469e8ba8ef294f6d47e0c47619b9ff95685
John Visser <100942775+Johnny63Vi@users.noreply.github.com>
2022-09-15 17:55:21 +0200

    Update and rename blink3b.cpp to blink3.cpp

--- blink3.cpp
+++ blink3.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define TIMELEDON2 2000000
 #define TIMELEDOFF 2000000
 
-int main2 (void)
+int main(void)
 {
   cout<<"hoi opgave2"<<endl;
  
@@ -29,25 +29,7 @@ int main2 (void)
 
 
   Logled logld1(&miniC,18, "Rood", "Pietje Puk",4);
-  Logled logld2(&miniC,23,2);
-
-
-  cout<<"De eigenaar van ld1="<<logld1.deEigenaar()<<endl;
-  cout<<"De eigenaar van ld1="<<logld2.deEigenaar()<<endl;
-
-  for (;;)
-  {
-    logld1.zetAan();
-    logld2.zetAan();
-     usleep(TIMELEDON);
-    logld1.zetUit();
-    logld2.zetUit();
-    usleep(TIMELEDOFF);
-    logld1.zetAan();
-    logld2.zetAan();
-     usleep(TIMELEDON2);
-    logld1.zetUit();
-    logld2.zetUit();
+  logld1.zetAan();
 
   }
   return 0 ;
